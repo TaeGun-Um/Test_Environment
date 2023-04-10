4de544460b9fd6af0a000b3012744df84b7da6ff
엄태건 <umtimos@naver.com>
2023-04-10 15:48:31 +0900

    개방주소법

---
+++ Algo/Chaining/Chaining.cpp
@@ -0,0 +1,74 @@
+﻿
+#include <iostream>
+
+typedef char* KeyType;
+typedef char* ValueType;
+
+typedef struct tagNode
+{
+    KeyType Key;
+    ValueType Value;
+
+    struct tagNode* Next;
+} Node;
+
+typedef Node* List;
+
+typedef struct tagHashTable
+{
+    int TableSize;
+    List* Table;
+} HashTable;
+
+ValueType CHT_Get(HashTable* HT, KeyType Key)
+{
+    int Address = CHT_Hash(Key, strlen(Key), HT->TableSize);
+    List TheList = HT->Table[Address];
+    List Target = nullptr;
+
+    if (TheList == nullptr)
+    {
+        return nullptr;
+    }
+    
+    while (strcmp(TheList->Key, Key) == 0)
+    {
+        Target = TheList;
+        break;
+    }
+
+    if (TheList->Next == nullptr)
+    {
+        return nullptr;
+    }
+    else
+    {
+        TheList = TheList->Next;
+    }
+
+    return Target->Value;
+}
+
+ValueType CHT_Set(HashTable* HT, KeyType Key, ValueType Value)
+{
+    int Address = CHT_Hash(Key, strlen(Key), HT->TableSize);
+    Node* NewNode = CHT_CreateNode(Key, Value);
+
+    if (HT->Table[Address] == nullptr)
+    {
+        HT->Table[Address] = NewNode;
+    }
+    else
+    {
+        List L = HT->Table[Address];
+        NewNode->Next;
+        HT->Table[Address] = NewNode;
+
+        printf("Collision occured : Key(%s), Address(%d)\n", Key, Address);
+    }
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ Algo/Division_Hash/Division_Hash.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+
+typedef int KeyType;
+typedef int ValueType;
+
+typedef struct tagNode
+{
+    KeyType Key;
+    ValueType Value;
+} Node;
+
+typedef struct tagHashTable
+{
+    int TableSize;
+    Node* Table;
+} HashTable;
+
+HashTable* SHT_CreateHashTable(int TableSize)
+{
+    HashTable* HT = (HashTable*)malloc(sizeof(HashTable));
+    HT->Table = (Node*)malloc(sizeof(Node) * TableSize);
+    HT->TableSize = TableSize;
+
+    return HT;
+}
+
+int SHT_Hash(KeyType Key, int TableSize)
+{
+    return Key % TableSize;
+}
+
+void SHT_Set(HashTable* HT, KeyType Key, ValueType Value)
+{
+    int Adress = SHT_Hash(Key, HT->TableSize);
+    HT->Table[Adress].Key = Key;
+    HT->Table[Adress].Value = Value;
+}
+
+ValueType SHT_Get(HashTable* HT, KeyType Key)
+{
+    int Adress = SHT_Hash(Key, HT->TableSize);
+    return HT->Table[Adress].Value;
+}
+
+void SHT_DestroyHashTable(HashTable* HT)
+{
+    free(HT->Table);
+    free(HT);
+}
+
+int main()
+{
+    HashTable* HT = SHT_CreateHashTable(193);
+
+    SHT_Set(HT, 418, 32114);
+    SHT_Set(HT, 9, 514);
+    SHT_Set(HT, 27, 8917);
+    SHT_Set(HT, 1031, 286);
+
+    printf("Key:%d, Value:%d\n", 418, SHT_Get(HT, 418));
+    printf("Key:%d, Value:%d\n", 9, SHT_Get(HT, 9));
+    printf("Key:%d, Value:%d\n", 27, SHT_Get(HT, 27));
+    printf("Key:%d, Value:%d\n", 1031, SHT_Get(HT, 1031));
+}
+
